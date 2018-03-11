1a7a7b1d2de6d6b00caad1659097aeb8bb7cbb34
Harsh P Bajaj <harsh.rocks@hotmail.com>
2018-03-11 16:44:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var ConversationV1 = require('watson-developer-cloud/conversation/v1');
-
+var pizzapi = require('dominos');
 var app = express();
-
+//context for every user information
 var contexts = [];
 
 app.get('/smssent', function (req, res) {
+ //what message is sent
   var message = req.query.Body;
   var number = req.query.From;
   var twilioNumber = req.query.To;
@@ -27,12 +28,12 @@ app.get('/smssent', function (req, res) {
   var conversation = new ConversationV1({
     username: '',
     password: '',
-    version_date: ConversationV1.VERSION_DATE_2016_09_20
+    version_date: ConversationV1.VERSION_DATE_2018_03_10
   });
 
   console.log(JSON.stringify(context));
   console.log(contexts.length);
-
+  //watson cloud library being used to interact with conversation
   conversation.message({
     input: { text: message },
     workspace_id: '',
@@ -47,74 +48,33 @@ app.get('/smssent', function (req, res) {
          } else {
            contexts[contextIndex].context = response.context;
          }
-
+        //we get a final intent now we know the order
          var intent = response.intents[0].intent;
          console.log(intent);
          if (intent == "done") {
-           //contexts.splice(contexts.indexOf({'from': number, 'context': response.context}),1);
+         //remove previous context
            contexts.splice(contextIndex,1);
-           // Call REST API here (order pizza, etc.)
-           //Input order from json
-console.log("Getting order from file...");
-var order = new pizzapi.Order(
-  orderconfig["order"]
-);
-//Add items to order
-console.log("Adding items to order...")
-var items = orderconfig["items"];
-for (var i=0; i<items.length; i++) {
-  order.addItem(
-    new pizzapi.Item(
-      items[i]
-    )
-  );
-}
-
-// Setup your Credit Card Info
-console.log("Setting up credit card info...")
-var cardInfo = new order.PaymentObject();
-cardInfo.Amount = order.Amounts.Customer;
-cardInfo.Number = orderconfig["cardNum"];
-cardInfo.CardType = order.validateCC(orderconfig["cardNum"]);
-cardInfo.Expiration = orderconfig["cardExp"];//  01/15 just the numbers "01/15".replace(/\D/g,'');
-cardInfo.SecurityCode = orderconfig["cardSec"];
-cardInfo.PostalCode = orderconfig["cardPost"]; // Billing Zipcode
-
-console.log("Adding card to order...");
-order.Payments.push(cardInfo);
-
-//TODO: if no mac address, find one and save it
-console.log("Searching for Dash Button...");
-var dash = dash_button(orderconfig["dashMacAddress"]);
-dash.on("detected", function (){
-    console.log("Dash Button Found");
-    console.log("Configuring Dash Button...");
-	//Validate, price, and place order!
-	order.validate(
-	    function(result) {
-	        console.log("Order is Validated");
-	    }
-	);
-	order.price(
-	    function(result) {
-            console.log("Order is Priced");
-	    }
-	);
-	order.place(
-	    function(result) {
-            console.log("Price is", result.result.Order.Amounts, "\nEstimated Wait Time",result.result.Order.EstimatedWaitMinutes, "minutes");
-	        console.log("Order placed!");
-	    }
-	);
-	console.log("Listening...");
-});
-         }
+           // Call npm library to order the pizza
+           //Input order from your message of order
+	console.log("Getting order from file...");
+	var order = new pizzapi.Order(message);
+	//Add items to order
+	console.log("Adding items to order...")
+	var items = message;
+	for (var i=0; i<items.length; i++) {
+	  order.addItem(
+	    new pizzapi.Item(
+	      items[i]
+	    )
+	  );
+	}
+        }
 
          var client = require('twilio')(
            '',
            ''
          );
-
+       //send watson response to customer
          client.messages.create({
            from: twilioNumber,
            to: number,
